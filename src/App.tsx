f048e94e34c5f6efd407885a78b463e10d6cb059
hyunyeee <5004ohy@naver.com>
2026-01-23 15:32:19 +0900

    feat: 신규 페이지 추가에 따른 라우팅 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./pages/main/HomePage.tsx";
-import PrivacySafePage from "./pages/main/PrivacySafePage.tsx";
-import ServiceGuidePage from "./pages/main/ServiceGuidePage.tsx";
-import LoginPage from "./pages/auth/LoginPage.tsx";
-import SignupPage from "./pages/auth/SignUpPage.tsx";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/main/HomePage";
+import PrivacySafePage from "./pages/main/PrivacySafePage";
+import ServiceGuidePage from "./pages/main/ServiceGuidePage";
+
+import LoginPage from "./pages/auth/LoginPage";
+import SignupPage from "./pages/auth/SignUpPage";
+
+import WaitingPage from "./pages/match/WaitingPage";
+import MatchRegisterPage from "./pages/match/MatchRegisterPage";
+import MatchedPage from "./pages/match/MatchedPage.tsx";
+import PostsPage from "./pages/community/PostsPage.tsx";
 
 export default function App() {
   return (
     <Routes>
-      {/* 메인페이지 */}
+      {/* Main */}
       <Route path="/" element={<HomePage />} />
       <Route path="/privacy-safe" element={<PrivacySafePage />} />
       <Route path="/service-guide" element={<ServiceGuidePage />} />
@@ -17,8 +24,19 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
 
+      {/* Match */}
+      <Route path="/match" element={<MatchRegisterPage />} />
+      <Route path="/waiting" element={<WaitingPage />} />
+      <Route path="/success" element={<MatchedPage />} />
+
+      {/* Posts  */}
+      <Route path="/posts" element={<PostsPage />} />
+      {/*<Route path="/posts/write" element={<PostWritePage />} />*/}
+      {/*<Route path="/posts/:postId" element={<PostDetailPage />} />*/}
+      {/*<Route path="/posts/:postId/edit" element={<PostEditPage />} />*/}
+
       {/* fallback */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      {/*<Route path="*" element={<Navigate to="/login" replace />} />*/}
     </Routes>
   );
 }
