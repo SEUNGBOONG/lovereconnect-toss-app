cafaf132a74e2caa89626150d7a4855ec95b453b
hyunyeee <5004ohy@naver.com>
2026-01-26 15:11:16 +0900

    feat: 수정, 삭제 액션 모달 추가

---
+++ src/components/overlay/modal/ActionModal.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@toss/tds-mobile";
+import { useOverlay } from "../../../hooks/useOverlay";
+
+interface Props {
+  onEdit?: () => void;
+  onDelete?: () => void;
+}
+
+export default function ActionModal({ onEdit, onDelete }: Props) {
+  const { closeOverlay } = useOverlay();
+
+  const handleEdit = () => {
+    closeOverlay();
+    onEdit?.();
+  };
+
+  const handleDelete = () => {
+    closeOverlay();
+    onDelete?.();
+  };
+
+  return (
+    <div className="flex w-full flex-col space-y-3">
+      {/* 수정 */}
+      <Button display="block" variant="weak" size="medium" onClick={handleEdit}>
+        수정
+      </Button>
+
+      {/* 삭제 */}
+      <Button
+        display="block"
+        variant="weak"
+        size="medium"
+        onClick={handleDelete}
+        className="!text-red-500"
+      >
+        삭제
+      </Button>
+
+      {/* 취소 */}
+      <Button display="block" variant="weak" size="medium" onClick={closeOverlay}>
+        취소
+      </Button>
+    </div>
+  );
+}
