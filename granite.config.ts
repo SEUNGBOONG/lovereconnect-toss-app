676fd15ffdc9527eff05e1a3b6d91e3fa1caeed6
hyunyeee <5004ohy@naver.com>
2026-01-30 16:21:12 +0900

    chore: 앱 아이콘 경로 수정

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: "러브리커넥트", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#d5356b", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "/icon.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "https://static.toss.im/appsintoss/10581/49a324de-330a-418a-b9fe-30398c3ea836.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
     bridgeColorMode: "basic",
   },
   web: {
@@ -14,6 +14,17 @@ export default defineConfig({
     commands: {
       dev: "pnpm dev",
       build: "pnpm build",
+    },
+  },
+  navigationBar: {
+    withBackButton: true,
+    withHomeButton: true,
+    initialAccessoryButton: {
+      id: "customer-center",
+      title: "고객센터",
+      icon: {
+        name: "icon-heart-mono",
+      },
     },
   },
   permissions: [],
